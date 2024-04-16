7e6e935d57d2a01f539bbd55d61ba64d3f86da34
Gino <gino.herrera@softvan.com.pe>
2024-04-16 01:20:19 -0500

    *a part of the home layout has been added

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,18 +1,81 @@
-//
 import React, { FC } from 'react'
 import { StackScreenProps } from '@react-navigation/stack'
 import { ParamListBase } from '@react-navigation/native'
 import ViewBase from '../../components/ViewBase'
-import { Typography } from '../../components/Typography'
+import {
+  FontColor,
+  FontSize,
+  FontWeight,
+  Typography,
+} from '../../components/Typography'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { gridUnits, heightPercent } from '../../utils/dimensions'
+import { COLORS } from '../../constants/colors'
 
 type HomeProps = StackScreenProps<ParamListBase>
 
 const Home: FC<HomeProps> = ({ navigation }) => {
+  const goToMenu = () => {
+    //ga
+  }
   return (
-    <ViewBase title={'hola0'}>
-      <Typography.AgrandirRegular>{'Probandin'}</Typography.AgrandirRegular>
+    <ViewBase contentStyle={styles.container}>
+      <View style={styles.nameContainer}>
+        <Typography.LatoSemiBold
+          lineHeight={FontSize.MD}
+          color={FontColor.WHITE}
+          style={{ fontWeight: FontWeight.W_800 }}>
+          {'Buenos Dias especial.\nUser'}
+        </Typography.LatoSemiBold>
+        <TouchableOpacity
+          onPress={goToMenu}
+          style={styles.circle}></TouchableOpacity>
+      </View>
+
+      <View style={styles.carouselContainer}></View>
+
+      <Typography.LatoSemiBold
+        lineHeight={FontSize.MD}
+        color={FontColor.WHITE}
+        style={{
+          textTransform: 'uppercase',
+          marginTop: gridUnits(2),
+          fontWeight: FontWeight.W_800,
+        }}>
+        {'health metric'}
+      </Typography.LatoSemiBold>
     </ViewBase>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  carouselContainer: {
+    backgroundColor: COLORS.WHITE,
+    borderRadius: gridUnits(4),
+    // flex: 1,
+    borderWidth: 1,
+    height: heightPercent(0.26),
+    padding: gridUnits(2),
+  },
+  circle: {
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 20,
+    height: 40,
+    top: 0,
+    width: 40,
+  },
+  container: {
+    backgroundColor: COLORS.BLACK,
+    flex: 1,
+    padding: gridUnits(3),
+  },
+  nameContainer: {
+    alignContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: gridUnits(2),
+  },
+})
