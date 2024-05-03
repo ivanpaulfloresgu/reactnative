1cb00e3bb675abcfec7725acbd2b6f6cd47e5a1d
LEONARDO <lamillarsotoleonardo@gmail.com>
2024-05-03 12:05:23 -0500

    se agrego audio mi king

--- src/views/leo/AudioSection.tsx
+++ src/views/leo/AudioSection.tsx
@@ -3,8 +3,39 @@ import { StackScreenProps } from '@react-navigation/stack'
 import { LeoStackParamList } from '../../navigation/LeoStack/LeoFlowScreen'
 import ViewBase from '../../components/ViewBase'
 import { COLORS } from '../../constants/colors'
+//import useSound from 'use-sound'
+import Sound from 'react-native-sound'
+import { ButtonBase } from '../../components/Button/ButtonBase'
+import { StyleSheet } from 'react-native'
+import { FontColor } from '../../components/Typography'
+
 type AudioProps = StackScreenProps<LeoStackParamList, 'AUDIO_SECTION'>
 export const AudioSection: FC<AudioProps> = ({ navigation }) => {
+  const PlaySound = () => {
+    var Eresono = new Sound('eresono.mp3', Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.log('failed to load the sound', error)
+        return
+      }
+      // loaded successfully
+      console.log(
+        'duration in seconds: ' +
+          Eresono.getDuration() +
+          'number of channels: ' +
+          Eresono.getNumberOfChannels(),
+      )
+
+      // Play the sound with an onEnd callback
+      Eresono.play(success => {
+        if (success) {
+          console.log('successfully finished playing')
+        } else {
+          console.log('playback failed due to audio decoding errors')
+        }
+      })
+    })
+  }
+
   return (
     <ViewBase
       style={{ flex: 1 }}
@@ -13,6 +44,19 @@ export const AudioSection: FC<AudioProps> = ({ navigation }) => {
       showLeftButton
       navigation={navigation}
       backgroundColor={COLORS.GRAY[100]}
-      title="Audio"></ViewBase>
+      title="Audio">
+      <ButtonBase.OutFill
+        containerStyle={styles.SpaceButton}
+        fillColor={COLORS.BLACK}
+        labelColor={FontColor.BLUE_100}
+        label="Sonar"
+        onPress={PlaySound}
+      />
+    </ViewBase>
   )
 }
+const styles = StyleSheet.create({
+  SpaceButton: {
+    marginVertical: 15,
+  },
+})
